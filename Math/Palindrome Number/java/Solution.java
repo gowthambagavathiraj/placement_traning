9e845c7cfd51b8f82bbe80e024fb16fa279e1152
Gowtham <gowthambagavathiraj@gmail.com>
2026-07-17 22:46:15 +0530

    feat: Add Palindrome Number

---
+++ Math/Palindrome Number/java/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        
+        int a=x;
+        int reversed=0;
+        while(x!=0){
+            int digit=x%10;
+            reversed=reversed*10+digit;
+            x=x/10;
+        }
+        if(reversed==a){
+            return true;
+        }
+       
+        return false;
+    }
+}
